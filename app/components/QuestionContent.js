49e98292ffefac09e4a39e0abf7aecadc194937f
Maciek <maciej.m.maslowski@gmail.com>
2017-08-02 15:34:46 +0200

    [wip] tweak IndividualQuestion

--- app/components/QuestionContent.js
+++ app/components/QuestionContent.js
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 
 import Container from '../styled-components/Container';
 
-const QuestionContent = () => <QuestionContainer>test</QuestionContainer>;
+const QuestionContent = () =>
+  <QuestionContainer>
+    All my patients with diabetes should see an ophthalmologist yearly for a
+    dialated eye examination- begining at diagnosis inpeople with type 2
+    diabetes, and after 5 years in people with type 1 diabetes after puberty.
+    Patients with know eye disease, symptoms of blurred vision in one eye, or
+    blind spots need to see their ophthalmologist more frequently.
+  </QuestionContainer>;
 
 export default QuestionContent;
 
@@ -13,4 +20,6 @@ const QuestionContainer = Container.extend`
   display: flex;
   justify-content: center;
   align-items: flex-start;
+  line-height: 1.25;
+  padding: 1rem;
 `;
